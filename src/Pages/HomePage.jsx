0df6a3fd0e628740591f4f7c8d58f82b4f6f728d
eman-NEMO <emanelkaser@gmail.com>
2024-07-30 01:35:40 +0300

    t

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import '../Styles/Style.css'
 import Header from '../Components/Header/Header'
 import Home from '../Components/Home/Home'
-
+import logo from '../Assets/openmoji_space-shuttle.svg'
+import { Helmet } from 'react-helmet'
 export default function HomePage() {
   return (
 
     <>
+  <Helmet>
+    <link rel="icon" href={logo} sizes="10x10" />
+        <title>Space</title>
+      </Helmet>
      <Header/>
      <Home/>
     </>
