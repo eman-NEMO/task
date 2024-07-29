c7b597347df75095e1714ce17f052545d64304d3
eman-NEMO <emanelkaser@gmail.com>
2024-07-30 01:18:34 +0300

    remove

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,29 +2,13 @@ import React from 'react'
 import '../Styles/Style.css'
 import Header from '../Components/Header/Header'
 import Home from '../Components/Home/Home'
-import { motion } from 'framer-motion'
+
 export default function HomePage() {
-  const pageTransition = {
-    in: {
-      opacity: 1,
-      x: 0
-    },
-    out: {
-      opacity: 0,
-    }
-  };
   return (
 
     <>
-    
-    <div className='back_img'>
-    <Header />
-
-
-      <Home/>
-      
-     </div>
-    
+     <Header/>
+     <Home/>
     </>
     
     
